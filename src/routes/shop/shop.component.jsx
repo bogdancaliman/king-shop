e5f41c3fc65200c8973c79c2e755b6011e25ca7d
Bogdan <bogdan.caliman112@gmail.com>
2023-08-13 16:14:52 +0300

    feat:added categoriesMap and populate the website with new products from firestore-firebase

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,18 +1,25 @@
-import { useContext } from "react";
+import { Fragment, useContext } from "react";
 
-import { ProductsContext } from "../../context/products.context";
+import { CategoriesContext } from "../../context/categories.context";
 import PorductCard from "../../components/product-card/product-card.component";
 
-import "./shop.styles.scss"
+import "./shop.styles.scss";
 
 const Shop = () => {
-  const { products } = useContext(ProductsContext);
+  const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <div className="products-container">
-      {products.map((product) => (
-        <PorductCard key={product.id} product={product} />
+    <Fragment>
+      {Object.keys(categoriesMap).map((title) => (
+        <Fragment key={title}>
+          <h2>{title}</h2>
+          <div className="products-container">
+            {categoriesMap[title].map((product) => (
+              <PorductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </Fragment>
       ))}
-    </div>
+    </Fragment>
   );
 };
 
